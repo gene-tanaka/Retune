11922c30e836c55969056d8a0afd5416ba23fcb3
Gene Tanaka <gene@datavant.com>
2023-11-28 22:36:40 -0800

    follower list functions

--- app/tabs/utils.ts
+++ app/tabs/utils.ts
@@ -2,7 +2,7 @@ import supabase from "../../supabase";
 
 export const getAllUsers = async () => {
   try {
-    const { data, error } = await supabase.from("users").select("*");
+    const { data, error } = await supabase.from("Users").select("*");
     if (error) throw error;
     return data;
   } catch (error) {
@@ -12,7 +12,7 @@ export const getAllUsers = async () => {
 
 export const createUser = async (user: any) => {
   try {
-    const { data, error } = await supabase.from("users").insert([user]);
+    const { data, error } = await supabase.from("Users").insert([user]);
     if (error) throw error;
     return data;
   } catch (error) {
@@ -23,7 +23,7 @@ export const createUser = async (user: any) => {
 export const updateUser = async (id: string, updates: any) => {
   try {
     const { data, error } = await supabase
-      .from("users")
+      .from("Users")
       .update(updates)
       .match({ id });
     if (error) throw error;
@@ -35,10 +35,69 @@ export const updateUser = async (id: string, updates: any) => {
 
 export const deleteUser = async (id: string) => {
   try {
-    const { data, error } = await supabase.from("users").delete().match({ id });
+    const { data, error } = await supabase.from("Users").delete().match({ id });
     if (error) throw error;
     return data;
   } catch (error) {
     console.log(error);
   }
 }
+
+export const getFollowerList = async (userId: string) => {
+  try {
+    const { data, error } = await supabase
+      .from('UserFollowers')
+      .select('Users(id, username, first_name, last_name, description)')
+      .eq('followee_id', userId);
+
+    if (error) throw error;
+    return data ? data.map(item => item.Users) : [];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+export const getFollowingList = async (userId: string) => {
+  try {
+    const { data, error } = await supabase
+      .from('UserFollowers')
+      .select('Users(id, username, first_name, last_name, description)')
+      .eq('follower_id', userId);
+
+    if (error) throw error;
+    return data ? data.map(item => item.Users) : [];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+export const followUser = async (followerId: string, followeeId: string) => {
+  try {
+    const { data, error } = await supabase
+      .from('UserFollowers')
+      .insert([
+        { followerId, followeeId }
+      ]);
+
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const unfollowUser = async (followerId: string, followeeId: string) => {
+  try {
+    const { data, error } = await supabase
+      .from('UserFollowers')
+      .delete()
+      .match({ followerId, followeeId });
+
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
