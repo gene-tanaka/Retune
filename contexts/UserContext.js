8e89b96142e847671e91130b4d16d4f23549a25c
Collin Jung <kel.n7j@gmail.com>
2023-12-04 15:26:20 -0800

    updating follower/following count in other profiles

--- contexts/UserContext.js
+++ contexts/UserContext.js
@@ -1,15 +1,15 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext } from "react";
 
 const UserContext = createContext(null);
 
 export const UserProvider = ({ children }) => {
-    const [loggedInUserId, setLoggedInUserId] = useState(1);
+  const [loggedInUserId, setLoggedInUserId] = useState(1);
 
-    return (
-        <UserContext.Provider value={{ loggedInUserId, setLoggedInUserId }}>
-            {children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider value={{ loggedInUserId, setLoggedInUserId }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export const useUser = () => useContext(UserContext);
