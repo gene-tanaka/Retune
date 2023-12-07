d100d69af6c51b8df5fbbc33a760d11f0e6e0ea2
Gene Tanaka <gene@datavant.com>
2023-12-06 16:33:09 -0800

    updatePosts

--- app/tabs/home/index.js
+++ app/tabs/home/index.js
@@ -10,6 +10,7 @@ import {
 import { useState, useEffect } from "react";
 import { useUser } from "../../../contexts/UserContext";
 import {
+  getFollowingList,
   getFollowingListIDs,
   getPostsByUserIds,
   getUsersByIds,
@@ -22,7 +23,7 @@ const MyPost = ({ data, usernames }) => {
   return (
     <Post
       key={post.id}
-      user={"@" + usernames[post.userId].username}
+      user={"@" + usernames[post.userId]?.username}
       imageUrl={post.imageUrl}
       caption={post.caption}
       preview={post.preview}
@@ -30,7 +31,7 @@ const MyPost = ({ data, usernames }) => {
       artist={post.artist}
       duration={post.duration}
       timestamp={post.timestamp}
-      profile={usernames[post.userId].profile}
+      profile={usernames[post.userId]?.profile}
     />
   );
 };
@@ -44,25 +45,26 @@ const MyStatusBar = ({ backgroundColor, ...props }) => (
 );
 
 export default function Page() {
-  const { loggedInUserId } = useUser();
-  const [following, setFollowing] = useState([loggedInUserId]);
+  const { loggedInUserId, loggedInFollowingProfiles, setLoggedInFollowingProfiles } = useUser();
   const [posts, setPosts] = useState(null);
   const [usernames, setUsernames] = useState(null);
-  const params = useLocalSearchParams();
+  const [lastKnownFollowingProfiles, setLastKnownFollowingProfiles] = useState(null);
 
   const fetchFollowing = async () => {
     try {
-      const followingIds = await getFollowingListIDs(loggedInUserId);
-      setFollowing((current) => current.concat([...followingIds]));
-      if (following.length > 0) {
-        const users = await getUsersByIds(following);
+      const following = await getFollowingList(loggedInUserId);
+      setLoggedInFollowingProfiles(following);
+      const followingIds = following.map((user) => user.id)
+      followingIds.push(loggedInUserId);
+      if (followingIds.length > 0) {
+        const users = await getUsersByIds(followingIds);
         const usernameMap = users.reduce((acc, user) => {
           acc[user.id] = { username: user.username, profile: user.profilePic };
           return acc;
         }, {});
         setUsernames(usernameMap);
 
-        const fetchedPosts = await getPostsByUserIds(following);
+        const fetchedPosts = await getPostsByUserIds(followingIds);
         const sortedPosts = fetchedPosts.sort((a, b) => {
           return new Date(b.timestamp) - new Date(a.timestamp);
         });
@@ -74,10 +76,14 @@ export default function Page() {
   };
 
   useEffect(() => {
-    if (posts === null || usernames === null) {
+    const hasFollowingProfilesChanged = loggedInFollowingProfiles !== lastKnownFollowingProfiles;
+    const shouldFetch = posts === null || usernames === null || hasFollowingProfilesChanged;
+
+    if (shouldFetch) {
       fetchFollowing();
+      setLastKnownFollowingProfiles(loggedInFollowingProfiles);
     }
-  }, [posts, usernames, params]);
+  }, [loggedInFollowingProfiles, posts, usernames]);
 
   return (
     <ImageBackground
